c672642ebadc4fbadf386816a4d7315aced4f5cb
AvaN0x <clement.ricatte57@gmail.com>
2020-05-25 23:57:54 +0200

    Dynamically adding a new line

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -26,12 +26,70 @@ namespace gtaOnlineManagement
 
             var ui = new Label();
 
-            ui.Content = "ui";
-            ui.Background = new SolidColorBrush(Colors.Orange);
+            //ui.Content = "ui";
+            //ui.Background = new SolidColorBrush(Colors.Orange);
 
-            ui.Foreground = new SolidColorBrush(Colors.Black);
+            //ui.Foreground = new SolidColorBrush(Colors.Black);
 
-            Casino.Children.Add(ui);
+            //Casino.Children.Add(ui);
+
+
+            var luckyWheel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal
+            };
+
+            //var label = new Label
+            //{
+            //    Content = "Casino Lucky Wheel",
+            //    Style = (Style) Application.Current.FindResource("LabelStyle")
+            //};
+
+            //var button = new Button
+            //{
+            //    Style = (Style) Application.Current.FindResource("CasinoButtonStyle"),
+            //    IsEnabled = false
+            //};
+
+            luckyWheel.Children.Add(new Label
+            {
+                Content = "Casino Lucky Wheel",
+                Style = (Style) Application.Current.FindResource("LabelStyle")
+            });
+
+            var button = new Button
+            {
+                Content = new Image
+                {
+                    Source = new BitmapImage(new Uri("icons/spin.png", UriKind.Relative)),
+                    Height = 16,
+                    Width = 16
+                },
+                Style = (Style) Application.Current.FindResource("CasinoButtonStyle"),
+                IsEnabled = false,
+            };
+            button.Click += new RoutedEventHandler(this.btn_close_Click);
+
+            luckyWheel.Children.Add(button);
+            luckyWheel.Children.Add(new TextBlock
+            {
+                Text = "hh/mm",
+                Style = (Style) Application.Current.FindResource("TextBlockStyle")
+            });
+
+            Casino.Children.Add(luckyWheel);
+
+
+            //<StackPanel Orientation = "Horizontal" Background = "#00a6ff" >
+            //    <Label Content = "Casino Lucky Wheel" Style = "{StaticResource LabelStyle}" ></ Label >
+            //     <Button Style = "{StaticResource CasinoButtonStyle}"
+            //           IsEnabled = "True" >
+            //          <Viewbox Width = "16" Height = "16" >
+            //             <Image Source = "icons/spin.png" />
+            //          </Viewbox >
+            //     </Button >
+            //     <TextBlock Text = "hh/mm" Style = "{StaticResource TextBlockStyle}" />
+            //</StackPanel>
         }
 
         private void Drag_MouseDown(object sender, MouseButtonEventArgs e)
