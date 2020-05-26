7a136c7dc267ee4d36bb19f203d9d0c638352ed2
AvaN0x <clement.ricatte57@gmail.com>
2020-05-26 15:16:56 +0200

    Buttons are working WIP

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -24,72 +24,8 @@ namespace gtaOnlineManagement
         {
             InitializeComponent();
 
-            var ui = new Label();
-
-            //ui.Content = "ui";
-            //ui.Background = new SolidColorBrush(Colors.Orange);
-
-            //ui.Foreground = new SolidColorBrush(Colors.Black);
-
-            //Casino.Children.Add(ui);
-
-
-            var luckyWheel = new StackPanel
-            {
-                Orientation = Orientation.Horizontal
-            };
-
-            //var label = new Label
-            //{
-            //    Content = "Casino Lucky Wheel",
-            //    Style = (Style) Application.Current.FindResource("LabelStyle")
-            //};
-
-            //var button = new Button
-            //{
-            //    Style = (Style) Application.Current.FindResource("CasinoButtonStyle"),
-            //    IsEnabled = false
-            //};
-
-            luckyWheel.Children.Add(new Label
-            {
-                Content = "Casino Lucky Wheel",
-                Style = (Style) Application.Current.FindResource("LabelStyle")
-            });
-
-            var button = new Button
-            {
-                Content = new Image
-                {
-                    Source = new BitmapImage(new Uri("icons/spin.png", UriKind.Relative)),
-                    Height = 16,
-                    Width = 16
-                },
-                Style = (Style) Application.Current.FindResource("CasinoButtonStyle"),
-                IsEnabled = false,
-            };
-            button.Click += new RoutedEventHandler(this.btn_close_Click);
-
-            luckyWheel.Children.Add(button);
-            luckyWheel.Children.Add(new TextBlock
-            {
-                Text = "hh/mm",
-                Style = (Style) Application.Current.FindResource("TextBlockStyle")
-            });
-
-            Casino.Children.Add(luckyWheel);
-
-
-            //<StackPanel Orientation = "Horizontal" Background = "#00a6ff" >
-            //    <Label Content = "Casino Lucky Wheel" Style = "{StaticResource LabelStyle}" ></ Label >
-            //     <Button Style = "{StaticResource CasinoButtonStyle}"
-            //           IsEnabled = "True" >
-            //          <Viewbox Width = "16" Height = "16" >
-            //             <Image Source = "icons/spin.png" />
-            //          </Viewbox >
-            //     </Button >
-            //     <TextBlock Text = "hh/mm" Style = "{StaticResource TextBlockStyle}" />
-            //</StackPanel>
+            Casino.Children.Add(new oneInDelay("Casino Lucky Wheel", new DateTime(1, 1, 1)).panel);
+            Casino.Children.Add(new oneInDelay("Test2", new DateTime(1, 1, 1)).panel);
         }
 
         private void Drag_MouseDown(object sender, MouseButtonEventArgs e)
---
+++ src/oneInDelay.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace gtaOnlineManagement
+{
+    class oneInDelay
+    {
+        // plus adapté aux supplies qu'au timer 24h pour le moment, faire une version ou il faut avoir l'app lancée, faire un bouton qui permet de reset les lignes
+        public String name { get; set; }
+        public DateTime duration { get; set; }
+        public StackPanel panel { get; set; }
+
+        public oneInDelay(String name, DateTime duration)
+        {
+            this.name = name.Trim();
+            this.duration = duration;
+            initStackPanel();
+        }
+
+        public void initStackPanel()
+        {
+            panel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal
+            };
+
+
+            panel.Children.Add(new Label
+            {
+                Content = name,
+                Style = (Style) Application.Current.FindResource("LabelStyle")
+            });
+
+            var button = new Button
+            {
+                Content = new Image
+                {
+                    Source = new System.Windows.Media.Imaging.BitmapImage(new Uri("icons/spin.png", UriKind.Relative)),
+                    Height = 16,
+                    Width = 16
+                },
+                Style = (Style) Application.Current.FindResource("CasinoButtonStyle"),
+                IsEnabled = true
+            };
+            button.Click += (sender, e) => 
+            {
+                Push();
+            };
+
+            panel.Children.Add(button);
+            panel.Children.Add(new TextBlock
+            {
+                Name = "timer",
+                Text = duration.ToString(),
+                Style = (Style) Application.Current.FindResource("TextBlockStyle")
+            });
+
+
+        }
+
+        public void Push()
+        {
+            //((TextBlock) panel.Children[2]).Text = "ui";
+            setWait();
+        }
+
+        public void setWait()
+        {
+            ((Button)panel.Children[1]).IsEnabled = false;
+            panel.Background = (Brush)(new System.Windows.Media.BrushConverter()).ConvertFromString("#26292f");
+        }
+
+        public void setNeedPush()
+        {
+            ((Button)panel.Children[1]).IsEnabled = true;
+            panel.Background = (Brush)(new System.Windows.Media.BrushConverter()).ConvertFromString("#00a6ff");
+        }
+    }
+}
